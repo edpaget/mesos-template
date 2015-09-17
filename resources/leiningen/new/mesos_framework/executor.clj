27037585186d965918320ed710288d2d08b2878c
Edward Paget <edward@cassetteta.pe>
2015-09-17 10:44:54 -0500

    Add empty methods with docs for unused callbacks

--- resources/leiningen/new/mesos_framework/executor.clj
+++ resources/leiningen/new/mesos_framework/executor.clj
@@ -6,6 +6,7 @@
   []
   (mesos/executor
    (launchTask [driver task-info]
+               ;; Invoked when a task has been launched on this executor (initiated via SchedulerDriver.launchTasks(java.util.Collection<OfferID>, java.util.Collection<Task-info>, Filters))
                (future (loop []
                          (println (Date.) "Hey Mesos!")
                          (Thread/sleep 2000)
@@ -13,4 +14,23 @@
                (mesos/send-status-update driver {:task-id (:task-id task-info)
                                                  :state :task-running}))
    (registered [driver executor-info framework-info slave-info]
-               (println slave-info))))
+               ;; Invoked once the executor driver has been able to successfully connect with Mesos.
+               (println slave-info))
+   (disconnected [driver]
+                 ;; Invoked when the executor becomes "disconnected" from the slave (e.g., the slave is being restarted due to an upgrade).
+                 )
+   (error [driver message]
+          ;; Invoked when a fatal error has occurred with the executor and/or executor driver.
+          )
+   (frameworkMessage [driver data]
+                     ;; Invoked when a framework message has arrived for this executor.
+                     )
+   (killTask [driver task-id]
+             ;; Invoked when a task running within this executor has been killed (via SchedulerDriver.killTask(TaskID)).
+             )
+   (reregistered [driver slave-info]
+                 ;; Invoked when the executor re-registers with a restarted slave.
+                 )
+   (shutdown [driver]
+             ;; Invoked when the executor should terminate all of it's currently running tasks.
+             )))
--- resources/leiningen/new/mesos_framework/scheduler.clj
+++ resources/leiningen/new/mesos_framework/scheduler.clj
@@ -47,10 +47,12 @@
   [scheduler-state task-launcher]
   (mesos/scheduler
    (statusUpdate [driver status]
+                 ;; Invoked when the status of a task has changed (e.g., a slave is lost and so the task is lost, a task finishes and an executor sends a status update saying so, etc).
                  (condp = (:state status)
                    :task-lost (swap! scheduler-state update-in [:to-launch] inc)
                    (println "[statusUpdate]" status)))
    (resourceOffers [driver offers]
+                   ;; Invoked when resources have been offered to this framework.
                    (doseq [offer offers]
                      (println "[resourceOffers]" offer)
                      (let [uuid (str (java.util.UUID/randomUUID))]
@@ -59,4 +61,28 @@
                          (let [tasks (task-launcher uuid offer)]
                            (mesos/launch-tasks driver (:id offer) tasks)
                            (swap! scheduler-state update-in [:to-launch] dec))
-                         (mesos/decline-offer driver (:id offer))))))))
+                         (mesos/decline-offer driver (:id offer))))))
+   (disconnected [driver]
+                 ;; Invoked when the scheduler becomes "disconnected" from the master (e.g., the master fails and another is taking over).
+                 )
+   (error [driver message]
+          ;; Invoked when there is an unrecoverable error in the scheduler or driver.
+          )
+   (executorLost [driver executor-id slave-id status]
+                 ;; Invoked when an executor has exited/terminated.
+                 )
+   (frameworkMessage [driver executor-id slave-id data]
+                     ;; Invoked when an executor sends a message.
+                     )
+   (offerRescinded [driver offer-id]
+                   ;; Invoked when an offer is no longer valid (e.g., the slave was lost or another framework used resources in the offer).
+                   )
+   (registered [driver framework-id masterInfo]
+               ;; Invoked when the scheduler successfully registers with a Mesos master.
+               )
+   (reregistered [driver masterInfo]
+                 ;; Invoked when the scheduler re-registers with a newly elected Mesos master.
+                 )
+   (slaveLost [driver slave-id]
+              ;; Invoked when a slave has been determined unreachable (e.g., machine failure, network partition).
+              )))
