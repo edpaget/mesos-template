4325b9d6d31a03dfd64cb0ee0adb8ace36e3dc4b
Edward Paget <edward@cassetteta.pe>
2015-09-18 09:16:42 -0500

    Add lein release configuration

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject mesos-framework/lein-template "0.1.0-SNAPSHOT"
+(defproject mesos-framework/lein-template "1.0.0-SNAPSHOT"
   :description "A template for setting up mesos-frameworks using component."
   :url "http://github.com/edpaget/mesos-framework"
+  :lein-release {:deploy-via :clojars}
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :eval-in-leiningen true)
